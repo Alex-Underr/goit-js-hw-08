848ec26a3db681b8b9139b9a06bd44f89dfe5fa3
Alex-underr <4ubs.pl@gmail.com>
2022-10-31 18:56:24 +0200

    P.1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
 // Change code below this line
 
-console.log(galleryItems);
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const containerImg = document.querySelector('.gallery');
+const createdPic = createImageEl(galleryItems);
+containerImg.insertAdjacentHTML('afterBegin', createdPic);
+
+function createImageEl(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+     <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+}
+
+containerImg.addEventListener('click', event => {
+    event.preventDefault();
+
+    let gallery = new SimpleLightbox('.gallery a',
+        {
+            captionsData: 'alt',
+            captionDelay: 300,
+        }
+    );
+})
